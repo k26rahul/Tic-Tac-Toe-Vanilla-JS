1cff4c5a7f3cd54e1b040dcf7eb454c3542d4eaf
Rahul Maurya <78677484+k26rahul@users.noreply.github.com>
2023-12-26 12:17:07 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,74 @@
+import { state, makeMove, resetState } from './ticTacToeGame.js';
+
+const playerXSymbol = 'X';
+const playerOSymbol = 'O';
+
+const statusElement = document.querySelector('.game-status');
+const cellElements = document.querySelectorAll('.grid-cell');
+
+document.querySelector('.restart-btn').addEventListener('click', () => {
+  if (state.isGameOver) restart();
+  else if (window.confirm('Are you sure you want to restart the game?'))
+    restart();
+});
+
+restart();
+function restart() {
+  cellElements.forEach((cellElement, index) => {
+    cellElement.textContent = '';
+    cellElement.className = 'grid-cell';
+    cellElement.addEventListener('click', () => handleCellClick(index), {
+      once: true,
+    });
+  });
+  resetState();
+  updateStatus();
+}
+
+function handleCellClick(index) {
+  if (!isMovePossible(index)) return;
+
+  updateCell(index);
+  makeMove(index);
+  updateStatus();
+
+  if (state.winner) {
+    highlightWinnerCells();
+  }
+}
+
+function updateCell(index) {
+  const cellElement = cellElements[index];
+  cellElement.textContent = getPlayerSymbol(state.currentPlayer);
+  cellElement.classList.add(state.currentPlayer);
+}
+
+function highlightWinnerCells() {
+  state.winningLine.forEach(index => {
+    cellElements[index].classList.add('winner');
+  });
+}
+
+function updateStatus() {
+  if (state.winner) {
+    setStatusText(`Player ${getPlayerSymbol(state.winner)} wins!`);
+    return;
+  }
+  if (state.isGameOver) {
+    setStatusText("Game over. It's a draw!");
+    return;
+  }
+  setStatusText(`Current move: Player ${getPlayerSymbol(state.currentPlayer)}`);
+}
+
+function isMovePossible(index) {
+  return !state.isGameOver && state.board[index] === null;
+}
+
+function getPlayerSymbol(player) {
+  return player === 'X' ? playerXSymbol : playerOSymbol;
+}
+
+function setStatusText(text) {
+  statusElement.textContent = text;
+}
---
+++ ticTacToeGame.js
@@ -0,0 +1,62 @@
+const winningLines = [
+  // Rows
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+
+  // Columns
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+
+  // Diagonals
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+const state = {
+  board: Array(9).fill(null),
+  currentPlayer: 'X',
+  winner: null,
+  winningLine: null,
+  isGameOver: false,
+};
+
+function resetState() {
+  state.board = Array(9).fill(null);
+  state.currentPlayer = 'X';
+  state.winner = null;
+  state.winningLine = null;
+  state.isGameOver = false;
+}
+
+function makeMove(index) {
+  state.board[index] = state.currentPlayer;
+  if (checkWinner() || isBoardFull()) {
+    state.isGameOver = true;
+  }
+  state.currentPlayer = state.currentPlayer === 'X' ? 'O' : 'X';
+}
+
+function checkWinner() {
+  for (const line of winningLines) {
+    const [a, b, c] = line;
+    if (
+      state.board[a] &&
+      state.board[a] === state.board[b] &&
+      state.board[a] === state.board[c]
+    ) {
+      state.winner = state.board[a];
+      state.winningLine = line;
+      return true;
+    }
+  }
+  return false;
+}
+
+function isBoardFull() {
+  return state.board.every(cell => cell !== null);
+}
+
+window.state = state;
+export { state, makeMove, resetState };
