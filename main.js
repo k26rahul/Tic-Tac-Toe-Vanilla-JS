1d7c3832d6aba46833895c6227bb5b2624360ca9
Rahul Maurya <78677484+k26rahul@users.noreply.github.com>
2023-12-29 10:06:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ import { state, makeMove, resetState } from './ticTacToeGame.js';
 import { displayExclusive } from './utils.js';
 window.state = state;
 
-const playerXSymbol = '✖️';
-const playerOSymbol = '⭕';
+const playerXSymbol = 'X';
+const playerOSymbol = 'O';
 let XScore = 0;
 let OScore = 0;
 let drawScore = 0;
