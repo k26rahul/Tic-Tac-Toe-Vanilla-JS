44eebf00cd130e607bbe955aa2a4000720b75aa0
Rahul Maurya <78677484+k26rahul@users.noreply.github.com>
2024-01-14 11:35:17 +0530

    Update game-grid.js

--- components/game-container/game-grid.js
+++ components/game-container/game-grid.js
@@ -13,7 +13,23 @@ html`<template>
     <button class="grid-cell"></button>
   </section>
   <script>
+      :root {
+      --body-bg-color: #c9edff;
+      --game-container-bg-color: beige;
+      --grid-lines-color: #00334c;
+      --winner-cell-bg-color: white;
+    }
+
+    * {
+      padding: 0;
+      margin: 0;
+      box-sizing: border-box;
+      font-family: inherit;
+    }
+
+    html,
     body {
+      height: 100%;
     }
   </script>
 </template>`;
