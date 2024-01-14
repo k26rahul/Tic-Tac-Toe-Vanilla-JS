e182cdc51574806e3b2519ee31d992f9cc0a0bea
Rahul Maurya <78677484+k26rahul@users.noreply.github.com>
2024-01-14 18:50:55 +0530

    *

--- components/game-container/game-grid.js
+++ components/game-container/game-grid.js
@@ -24,6 +24,8 @@ css`
   }
 
   body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     display: grid;
     grid-auto-flow: column;
     justify-content: center;
