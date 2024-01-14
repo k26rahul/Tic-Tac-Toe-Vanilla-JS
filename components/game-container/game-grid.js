ca1d7f5841e51513db334485bc978349485a1fb7
Rahul Maurya <78677484+k26rahul@users.noreply.github.com>
2024-01-14 11:06:59 +0530

    Component test

---
+++ components/game-container/game-grid.js
@@ -0,0 +1,13 @@
+html`<template>
+  <section class="game-grid">
+    <button class="grid-cell"></button>
+    <button class="grid-cell"></button>
+    <button class="grid-cell"></button>
+    <button class="grid-cell"></button>
+    <button class="grid-cell"></button>
+    <button class="grid-cell"></button>
+    <button class="grid-cell"></button>
+    <button class="grid-cell"></button>
+    <button class="grid-cell"></button>
+  </section>
+</template>`;
